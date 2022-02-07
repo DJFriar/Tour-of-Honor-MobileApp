1b74eb5b2fd915112aa7222e0e80edb792c4de04
DJFriar <tommy.craft@icloud.com>
2022-02-06 19:55:50 -0600

    added version info to account screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Application from 'expo-application';
+import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
 import AppButton from '../components/AppButton';
 import AppText from '../components/AppText';
@@ -8,55 +10,84 @@ import colors from '../config/colors';
 import Screen from '../components/Screen';
 import useAuth from '../auth/useAuth';
 
+
 function AccountScreen(props) {
   const { user, logOut } = useAuth();
+  const appVersion = Application.nativeApplicationVersion;
+  const buildNum = Application.nativeBuildVersion;
+  const tabBarHeight = useBottomTabBarHeight();
+  console.log(tabBarHeight);
+
 
   return (
-    <Screen style={styles.container}>
-      <Text style={styles.riderName}>{user.FirstName} {user.LastName}</Text>
-      <Text style={styles.flag}>Flag #{user.FlagNumber}</Text>
-      <Text>&nbsp;</Text>
-      <Text style={styles.text}>Zip Code: {user.ZipCode}</Text>
-      <Text style={styles.text}>Email: {user.Email}</Text>
-      <View style={styles.changesTextContainer}>
-        <AppText>To make changes to your User Profile, please login to the Scoring Portal.</AppText>
-      </View>
-      <View style={styles.logoutButtonContainer}>
-        <AppButton title="Log Out" onPress={() => logOut()}/>
-      </View>
-      <View style={styles.faqSection}>
-        <AppText>Wondering what all the icons are on the memorial details? Below is a handy legend:</AppText>
-        <View style={styles.iconDefinitionRow}>
-          <MaterialCommunityIcons name='clock-outline' size={25} style={[styles.icon, {color: 'black'}]} />
-          <Text style={styles.iconDefinition}>This memorial has been submitted and is awaiting review.</Text>
+    <Screen style={styles.screen}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between', flexDirection: 'column' }}>
+        <View style={styles.topContainer}>
+          <Text style={styles.riderName}>{user.FirstName} {user.LastName}</Text>
+          <Text style={styles.flag}>Flag #{user.FlagNumber}</Text>
+          <Text>&nbsp;</Text>
+          <Text style={styles.text}>Zip Code: {user.ZipCode}</Text>
+          <Text style={styles.text}>Email: {user.Email}</Text>
+          <View style={styles.changesTextContainer}>
+            <AppText>To make changes to your User Profile, please login to the Scoring Portal.</AppText>
+          </View>
+          <View style={styles.logoutButtonContainer}>
+            <AppButton title="Log Out" onPress={() => logOut()}/>
+          </View>
+          <View style={styles.faqSection}>
+            <AppText style={{marginBottom: 10}}>Wondering what all the icons are on the memorial details? Below is a handy legend:</AppText>
+            <View style={styles.iconDefinitionRow}>
+              <MaterialCommunityIcons name='clock-outline' size={25} style={[styles.icon, {color: 'black'}]} />
+              <Text style={styles.iconDefinition}>This memorial has been submitted and is awaiting review.</Text>
+            </View>
+            <View style={styles.iconDefinitionRow}>
+              <MaterialCommunityIcons name='shield-check' size={25} style={[styles.icon, {color: 'green'}]} />
+              <Text style={styles.iconDefinition}>This memorial has been scored and approved. Congrats!</Text>
+            </View>
+            <View style={styles.iconDefinitionRow}>
+              <MaterialCommunityIcons name='image-multiple-outline' size={25} style={[styles.icon, {color: 'black'}]} />
+              <Text style={styles.iconDefinition}>This memorial requires two images. See the official rules for more details.</Text>
+            </View>
+            <View style={styles.iconDefinitionRow}>
+              <MaterialCommunityIcons name='alert-octagon-outline' size={25} style={[styles.icon, {color: 'red'}]} />
+              <Text style={styles.iconDefinition}>This memorial has a restriction. Scroll down the details to see what it is.</Text>
+            </View>
+            <View style={styles.iconDefinitionRow}>
+              <MaterialCommunityIcons name='map-search-outline' size={25} style={[styles.icon, {color: 'black'}]} />
+              <Text style={styles.iconDefinition}>Tap on this icon to get driving directions using your phone's default map app.</Text>
+            </View>
+          </View>
         </View>
-        <View style={styles.iconDefinitionRow}>
-          <MaterialCommunityIcons name='shield-check' size={25} style={[styles.icon, {color: 'green'}]} />
-          <Text style={styles.iconDefinition}>This memorial has been scored and approved. Congrats!</Text>
+        <View style={styles.bottomContainer}>
+          <View style={styles.appInfoRow}>
+            <Text style={styles.copyright}>&copy;2022 ambitiousNerds</Text>
+            <Text style={styles.appInfo}>Version {appVersion}</Text>
+          </View>
         </View>
-        <View style={styles.iconDefinitionRow}>
-          <MaterialCommunityIcons name='image-multiple-outline' size={25} style={[styles.icon, {color: 'black'}]} />
-          <Text style={styles.iconDefinition}>This memorial requires two images. See the official rules for more details.</Text>
-        </View>
-        <View style={styles.iconDefinitionRow}>
-          <MaterialCommunityIcons name='alert-octagon-outline' size={25} style={[styles.icon, {color: 'red'}]} />
-          <Text style={styles.iconDefinition}>This memorial has a restriction. Scroll down the details to see what it is.</Text>
-        </View>
-        <View style={styles.iconDefinitionRow}>
-          <MaterialCommunityIcons name='map-search-outline' size={25} style={[styles.icon, {color: 'black'}]} />
-          <Text style={styles.iconDefinition}>Tap on this icon to get driving directions using your phone's default map app.</Text>
-        </View>
-      </View>
+      </ScrollView>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.background,
+  appInfo:{
+    fontSize: 12,
+  },
+  appInfoRow:{
+    alignItems: 'flex-end',
     flex: 1,
-    paddingHorizontal: 10,
-    paddingTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 4
+    // marginBottom: Platform.OS === "android" ? 32 : 22,
+  },
+  bottomContainer: {
+    // flex: 1,
+    // justifyContent: 'flex-end',
+    marginTop: 40,
+  },
+  copyright: {
+    fontSize: 10,
   },
   changesTextContainer: {
     margin: 10
@@ -86,8 +117,16 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "500",
   },
+  screen: {
+    backgroundColor: colors.background,
+  },
   text: {
     fontSize: 18,
+  },
+  topContainer: {
+    flex: 2,
+    paddingHorizontal: 10,
+    paddingTop: 15,
   },
 });
 
