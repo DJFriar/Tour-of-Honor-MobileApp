8bae5616ab391bba71f046bf298c4c73deef445c
DJFriar <tommy.craft@icloud.com>
2022-02-10 00:14:13 -0600

    Disabled zooming the text in, but can still zoom text out.

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -5,7 +5,7 @@ import defaultStyles from '../config/styles';
 
 function AppText({children, style, ...otherProps }) {
   return (
-    <Text style={[defaultStyles.text, style]} {...otherProps }>{children}</Text>
+    <Text style={[defaultStyles.text, style]} {...otherProps } maxFontSizeMultiplier={1}>{children}</Text>
   );
 }
 
